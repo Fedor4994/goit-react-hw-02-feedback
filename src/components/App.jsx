2a4961b76a080c0312841d9b24970c9243e78516
Fedor4994 <fedorsosnin04@gmail.com>
2022-11-19 06:43:26 +0200

    Add started files

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: 20,
+        padding: 40,
+      }}
+    >
+      
+      <h2>Task 1</h2>
+      
+      
+      </div>
+  );
+};
